e39f3897e862c83ab5b74d0815f4c60be278d427
Ahmedsabrymohamed74 <ahmad.s.abdelaziz@gmail.com>
2021-07-31 21:12:18 +0200

    Addition of Local client
    Timeout Optimization.

--- BLE_scan.ino
+++ BLE_scan.ino
@@ -18,9 +18,9 @@
 #include <Ubidots.h>
 
 /* Network credentials*/
-const char* UBIDOTS_TOKEN = "UBIDOTS token";
-const char* WIFI_SSID = "ssid";
-const char* WIFI_PASS = "pass"; 
+const char* UBIDOTS_TOKEN = "BBFF-yRZeguGTJQXLKQIx2cOzEkEsWTQhk0";
+const char* WIFI_SSID = "Dr.Sabry's Network (tmp)"; //VodafoneMobileWiFi-A04C95
+const char* WIFI_PASS = "$Drsabryhome01"; //9877350427
 //Ubidots ubidots(UBIDOTS_TOKEN, UBI_TCP);
 Ubidots ubidots(UBIDOTS_TOKEN, UBI_TCP);
 // Ubidots ubidots(UBIDOTS_TOKEN, UBI_UDP);
@@ -31,25 +31,25 @@ BLEScan* pBLEScan;
 
 class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
     void onResult(BLEAdvertisedDevice advertisedDevice) {
-
+      
       /* Sending to Ubidots*/
       bool foo = true;
       while (foo == true) {
-        ubidots.add("Test MAC", advertisedDevice.getRSSI());
+        ubidots.addContext("MAC Address", advertisedDevice.getAddress().toString().c_str());
+        char* context = (char*)malloc(sizeof(char) * 60); //Reserves memory to store context array
+        ubidots.getContext(context); //Builds the context with the array above to send to Ubidots
+        ubidots.add("Device", advertisedDevice.getRSSI(), context);
         bool bufferSent = false;
         bufferSent = ubidots.send();
         delay(7500);
 
         if (bufferSent) {
           Serial.println("Values sent by the device");
-          ESP.restart();
         }
-        else {
-          ESP.restart();
-        }
+        ESP.restart();
+        free(context);
 
-
-        /*           Printing MACs and resp RSSI
+        /*           Printing MACs and resp RSSI in Serial
           Serial.print("MAC Address: ");
           Serial.println(advertisedDevice.getAddress().toString().c_str());
           Serial.print("RSSI: ");
@@ -64,7 +64,7 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
 void setup() {
   Serial.begin(115200);
   ubidots.wifiConnect(WIFI_SSID, WIFI_PASS);
-  ubidots.setDebug(true);  // Uncomment to debug
+  ubidots.setDebug(true);
 
   Serial.println("Starting Scan...");
   BLEDevice::init("ESP32");
